b9f85ae1fd211a9be412d749feea38ee53e0bae3
Taybah Tahir <taybah.tahir@madetech.com>
2026-01-09 23:07:48 +0000

    sign up buttons inputted, linked log in page tgther

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+def get_db():
+    db = sqlite3.connect("database.db")
+    db.row_factory = sqlite3.Row
+    return db
+
+
+@app.route("/")
+def home():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+    
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method =="GET":
+        return render_template("login.html")
+    
+    if request.method =="POST":
+        
+        db = get_db()
+        data = request.get_json()
+    
+        email = data["email"]
+        password = data["password"]
+    
+        # learn this bro
+        cursor = db.execute("SELECT * FROM users WHERE email = ? AND password = ?", (email, password))
+        user = cursor.fetchone()
+        
+        if user:
+            return jsonify({"valid": True})
+        else:
+            return jsonify({"valid": False})
+    
+if __name__ == "__main__":
+    app.run(debug=True)
